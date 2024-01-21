301dda5d424f88c4b20fabc0cf1c6ded192552fb
najatmerabet <najatelmrabet2001@gmail.com>
2024-01-21 13:49:29 +0000

    nepastoucher

--- frontend/src/app/components/cardformation/cardformation.component.ts
+++ frontend/src/app/components/cardformation/cardformation.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {FormationService} from 'src/app/shared/services/formation.service';
-import { FormationModule } from 'src/app/Module/formation/formation.module';
+
 @Component({
   selector: 'app-cardformation',
   templateUrl: './cardformation.component.html',
   styleUrls: ['./cardformation.component.css']
 })
 export class CardformationComponent implements OnInit {
-Formationdata:FormationModule []=[]
+
  constructor(private formationservice:FormationService){}
 
 ngOnInit(): void {
@@ -18,15 +18,6 @@ ngOnInit(): void {
 
 public showformation() {
   console.log("je peux ")
-  this.formationservice.afficherFormation().subscribe(
-    (res: FormationModule[]) => {
-      console.log ("afficheer")
-      this.Formationdata = res;
-    },
-    (err: HttpErrorResponse) => {
-      console.log("ne peux pas")
-      console.log(err);
-    }
-  );
+ 
 }
 }
