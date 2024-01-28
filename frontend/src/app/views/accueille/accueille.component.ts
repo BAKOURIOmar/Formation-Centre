df0550cd9d9cb6f762968e63f34ebfb1b45c2b31
BeneichAbir <abirbeneich21@gmail.com>
2024-01-28 20:19:05 +0100

    modification des formulaire

--- frontend/src/app/views/accueille/accueille.component.ts
+++ frontend/src/app/views/accueille/accueille.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accueille',
@@ -12,4 +12,27 @@ export class AccueilleComponent {
 throw new Error('Method not implemented.');
 }
 
+}*/
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-accueille',
+  templateUrl: './accueille.component.html',
+})
+export class AccueilleComponent {
+
+  constructor(public dialog: MatDialog) { }
+
+  openLoginForm(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Le dialogue est fermé');
+    });
+  }
 }
+
