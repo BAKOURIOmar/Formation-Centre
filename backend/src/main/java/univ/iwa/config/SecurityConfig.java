3fb4d69afb5d3c544149f3032f0118cccd7996bd
Omar BAKOURI <omar.bakouri@babelgroup.com>
2024-02-05 20:11:24 +0100

    fixing errors

--- backend/src/main/java/univ/iwa/config/SecurityConfig.java
+++ backend/src/main/java/univ/iwa/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
 								.requestMatchers("/form/getformations").permitAll()
 								.requestMatchers("/group/getAllGroupes").authenticated()
 								.requestMatchers("/plan/getPlanifications").authenticated()
-								.requestMatchers("/auth/usersByName").authenticated())
+								.requestMatchers("/auth/usersByName").authenticated()
 								.requestMatchers("/plan/getFormateurPlanifications/**").authenticated()
 								)
 		
